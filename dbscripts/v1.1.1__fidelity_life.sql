c84ce0e173980bb7c95481dfc36be2bb7af13608
loicbi <loic.andre.bi@gmail.com>
2023-06-01 14:03:02 -0400

    schema change 1.0

--- dbscripts/v1.1.1__fidelity_life.sql
+++ dbscripts/v1.1.1__fidelity_life.sql
@@ -1,8 +1,1 @@
 use database demo_db;
-use schema schemachange;
-Select 'abc';
-create table fidelity_test_users (
-  name varchar (100),  -- variable string column
-  preferences string, -- column used to store JSON type of data
-  created_at timestamp
-);
